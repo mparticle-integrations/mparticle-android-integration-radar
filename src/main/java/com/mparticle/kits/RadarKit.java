a0b3227d474b9cd3a4f39289b0695df0a6fdeb6b
wpassidomo <wpassidomo@mparticle.com>
2020-07-01 14:24:09 -0400

    Update runtime name

--- src/main/java/com/mparticle/kits/RadarKit.java
+++ src/main/java/com/mparticle/kits/RadarKit.java
@@ -67,7 +67,7 @@ public class RadarKit extends KitIntegration implements KitIntegration.Applicati
 
     @Override
     public String getName() {
-        return "Radar";
+        return "RadarKit";
     }
 
     @Override
